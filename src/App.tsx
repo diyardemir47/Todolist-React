b66d38d54354c81dac798d2307869f87290be11d
Diyar Demir <99801830+diyardemir47@users.noreply.github.com>
2023-12-22 17:12:31 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
+// App.tsx
+import React, { ChangeEvent, FC, useState } from 'react';
 import './App.css';
+import { todoType } from './apptypes';
+import { TodoItem } from './TodoItem';
 
-function App() {
+const App: FC = () => {
+  const [task, setTask] = useState<string>('');
+  const [workday, setWorkday] = useState<number>(0);
+  const [todolist, setTodolist] = useState<todoType[]>([]);
+  const [completedTasks, setCompletedTasks] = useState<string[]>([]); // Yeni eklenen state
+
+  const handlechange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value);
+    } else {
+      setWorkday(Number(event.target.value));
+    }
+  };
+
+  const addNewTask = () => {
+    const newTask = { taskName: task, workday: workday, completed: false }; // 'completed' özelliği eklenmiştir
+    setTodolist([...todolist, newTask]);
+    setTask('');
+    setWorkday(0);
+  };
+
+  const deleteTask = (nameToDelete: string) => {
+    setTodolist(todolist.filter((task) => task.taskName !== nameToDelete));
+  };
+
+  const updateWorkday = (nameToUpdate: string, newWorkday: number) => {
+    setTodolist((prevTodolist) =>
+      prevTodolist.map((task) =>
+        task.taskName === nameToUpdate ? { ...task, workday: newWorkday } : task
+      )
+    );
+  };
+
+  const updateTaskName = (nameToUpdate: string, newTaskName: string) => {
+    setTodolist((prevTodolist) =>
+      prevTodolist.map((task) =>
+        task.taskName === nameToUpdate ? { ...task, taskName: newTaskName } : task
+      )
+    );
+  };
+
+
+  const markAsCompleted = (nameToUpdate: string) => {
+    setTodolist((prevTodolist) =>
+      prevTodolist.map((task) =>
+        task.taskName === nameToUpdate ? { ...task, completed: true } : task
+      )
+    );
+    setCompletedTasks((prevCompletedTasks) => [...prevCompletedTasks, nameToUpdate]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <input
+          className="input"
+          type="text"
+          placeholder="Taskınızı giriniz.."
+          value={task}
+          name="task"
+          onChange={handlechange}
+        />
+        <input
+          className="input"
+          type="number"
+          placeholder="Kaç günde tamamlamalısınız"
+          value={workday}
+          name="number"
+          onChange={handlechange}
+        />
+        <button onClick={addNewTask} className="ekle">
+          Yeni Task Ekle
+        </button>
+
+        {todolist.map((task: todoType, index: number) => (
+          <TodoItem
+            key={index}
+            task={task}
+            deleteTask={deleteTask}
+            updateWorkday={updateWorkday}
+            updateTaskName={updateTaskName}
+            markAsCompleted={markAsCompleted} // 'markAsCompleted' fonksiyonu eklenmiştir
+          />
+        ))}
+
+
+      </div> <div className="completed-tasks">
+        <h2>Tamamlanan Görevler</h2>
+        <ul>
+          {completedTasks.map((completedTask, index) => (
+            <li key={index}>{completedTask}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
